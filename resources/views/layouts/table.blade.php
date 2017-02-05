13846683c8bae5a1b6c1605212e613d8b322a843
MatthieuJnon <mattjoannon@gmail.com>
2017-02-05 16:52:23 +0100

    minor fixes in vehicles.show

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -1,30 +1,19 @@
 <table class ="vehicles-table">
 	<tr>
 		<td> id </td>
-		<td> First Name </td>
-		<td> Last Name </td>
-		<td> Contact Number </td>
-		<td> email </td>
+		<td> Owner name</td>
 		<td> manufacturer </td>
 		<td> type </td>
-		<td> year </td>
-		<td> colour </td>
-		<td> mileage </td>
 		<td> created at </td>
 		<td> edit </td>
 		<td> delete </td>
 	</tr>
 	@foreach($vehicles as $vehicle)
 		<tr>
-			@foreach($vehicle->toArray() as $info)
-				@if($loop->first)
-					<td><a href="{{ url('/vehicles',[$vehicle->id]) }}">{{ $info }}</a></td>
-				@elseif($loop->iteration >= 11)
-					@continue
-				@else
-					<td> {{ $info }} </td>
-				@endif
-			@endforeach
+			<td><a href="{{ url('/vehicles',[$vehicle->id]) }}">{{$vehicle->id}}</a></td>
+			<td> {{ $vehicle->first_name }} {{$vehicle->last_name}} </td>
+			<td> {{$vehicle->manufacturer}} </td>
+			<td> {{$vehicle->type}} </td>
 			<td> {{$vehicle->created_at}} </td>
 			<td><a href="vehicles/{{$vehicle->id}}/edit"> edit </a> </td>
 			<td><a href="{{ url('/vehicles/delete',[$vehicle->id]) }}" > delete </a></td>
